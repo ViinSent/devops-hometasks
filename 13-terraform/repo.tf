05bca99941d5557fd000421a2973a0a57ff1b878
Terraform User <viinsent@gmail.com>
2024-02-02 00:13:32 +0300

    Managed by Terraform

---
+++ 13-terraform/repo.tf
@@ -0,0 +1,15 @@
+resource "github_repository_file" "file" {
+
+  for_each = {for file in var.files : file => file}
+
+  repository = "devops-hometasks"
+  branch     = "main"
+  file       = "13-terraform/${each.value}"
+  content    = file("${path.module}/${each.value}")
+  #  content    = "new files"
+
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "viinsent@gmail.com"
+  overwrite_on_create = true
+}
